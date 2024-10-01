b64c2924a8cf88c2f067a91110e87217e27bd88a
GaiaMom <gaia.mom.first@gmail.com>
2024-10-01 01:13:48 -0700

    Hash Generation 1.1 batch insertion

--- backend/hash/hash_lib.js
+++ backend/hash/hash_lib.js
@@ -1,9 +1,19 @@
 var crypto = require('crypto')
 var db = require('../utils/db')
-exports.genGameHash = function(game, serverSeed, public_seed, gameId) {
-    return crypto.createHash('sha256').update(serverSeed + '-' + public_seed + '-' + gameId)
+exports.genGameHash = function (game, serverSeed, public_seed, gameId) {
+    return crypto.createHash('sha256').update(serverSeed + '-' + public_seed + '-' + gameId).digest('hex')
 }
-exports.insertHash = function(game, game_id, hash, callback) {
+exports.insertHashBatch = function (game, batch, callback) {
+    const values = batch.map(item => `(${db.con.escape(item.game_id)}, ${db.con.escape(item.hash)})`).join(',');
+    return db.cmd(db.statement("insert into", game != null && game.length ? game + '_game_hashes' : 'game_hashes', "(GAME_ID, HASH)", "", ` VALUES ${values}`)).then(res => {
+        if (res.success) {
+            callback(null, null)
+        } else {
+            callback(err)
+        }
+    })
+}
+exports.insertHash = function (game, game_id, hash, callback) {
     let items = [game_id, "'" + hash + "'"]
     db.cmd(db.statement("insert into", game != null && game.length ? game + '_game_hashes' : 'game_hashes', "(GAME_ID, HASH)", "", "VALUES (" + items.join(',') + ")")).then(res => {
         if (res.success) {
--- backend/hash/populate_hashes.js
+++ backend/hash/populate_hashes.js
@@ -4,30 +4,46 @@ var engine = require('./hash_lib')
 var cfg = require('./config.js')
 var gmId = cfg.maxCnt;
 var svrSeed = `x2bet_tarobet_${cfg.gmName}_nobody_does_not_know_chain_${new Date().toString('yyyy_MM_DD')}`
-function loop(cb) {
-    var parallel = Math.min(gmId, 1000);
-    var inserts = _.range(parallel).map(function () {
-        return function (cb) {
+async function loop(cb) {
+    while (gmId > 0) {
+        const dataToInsert = [];
+        for (let i = 0; i < 1000; i++) {
             gmId--;
             svrSeed = engine.genGameHash(cfg.gmName, svrSeed, cfg.pubSeed, gmId + 1)
-            engine.insertHash(/*cfg.gmName*/ '', gmId + 1, svrSeed, cb)
+            dataToInsert.push({ game_id: gmId + 1, hash: svrSeed });
         }
-    })
-    async.parallel(inserts, function (err) {
-        if (err) throw err;
+        await engine.insertHashBatch(/*cfg.gmName*/ '', dataToInsert, cb);
+
         var pct = 100 * (cfg.maxCnt - gmId) / cfg.maxCnt;
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
-        process.stdout.write("Processed: " + (cfg.maxCnt - gmId) + ' / ' + cfg.maxCnt + ' (' + pct.toFixed(2) + '%)')
-        if (gmId > 0) {
-            loop(cb)
-        } else {
-            console.log(' Done');
-            cb();
-        }
-    })
-    console.log(cfg.gmName + ' TotalCnt(' + cfg.maxCnt + ') \n' + 'PublicSeed: ' + cfg.pubSeed + '\nServerSeed: ' + svrSeed)
-    loop(function() {
-        console.log('Finished with serverseed: ', svrSeed)
-    })
+        process.stdout.write('\x1b[2K'); // Clear the current line
+        process.stdout.write(`\rProcessed: ${cfg.maxCnt - gmId} / ${cfg.maxCnt} (${pct}%) `);
+    }
+    console.log(' Done');
+    cb();
+
+
+    // var inserts = _.range(parallel).map(function () {
+    //     return function (cb) {
+    //         gmId--;
+    //         svrSeed = engine.genGameHash(cfg.gmName, svrSeed, cfg.pubSeed, gmId + 1)
+    //         engine.insertHash(/*cfg.gmName*/ '', gmId + 1, svrSeed, cb)
+    //     }
+    // })
+
+    // async.parallel(inserts, function (err) {
+    //     if (err) throw err;
+    //     var pct = 100 * (cfg.maxCnt - gmId) / cfg.maxCnt;
+    //     process.stdout.write('\x1b[2K'); // Clear the current line
+    //     process.stdout.write(`\rProcessed: ${cfg.maxCnt - gmId} / ${cfg.maxCnt} (${pct}%) `);
+
+    //     if (gmId > 0) {
+    //         loop(cb)
+    //     } else {
+    //         console.log(' Done');
+    //         cb();
+    //     }
+    // })
 }
+loop(function () {
+    console.log('Finished with serverseed: ', svrSeed)
+})
